60e6e249694ce3c8b4ad0d71d4bcaac70e906bfc
Patrick Menzel <patrick@modeanalytics.com>
2019-04-10 16:49:11 -0700

    Revert change to original implementation.

--- salt/modules/dockermod.py
+++ salt/modules/dockermod.py
@@ -1302,9 +1302,11 @@ def images(verbose=False, **kwargs):
             if img_id is None:
                 continue
             for item in img:
-                img_state = 'untagged' \
-                    if img['RepoTags'] == ['<none>:<none>'] \
-                    else 'tagged'
+                img_state = ('untagged' if
+                             img['RepoTags'] in (
+                               ['<none>:<none>'],  # docker API <1.24
+                               None,  # docker API >=1.24
+                             ) else 'tagged')
                 bucket = __context__.setdefault('docker.images', {})
                 bucket = bucket.setdefault(img_state, {})
                 img_key = key_map.get(item, item)
